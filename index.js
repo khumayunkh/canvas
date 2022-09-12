599850e2fbd4f6ac355ff8c039c914f73bf9b795
khumayunkh <hasanovhumayun15@gmail.com>
2022-09-12 22:12:45 +0500

    line

--- index.js
+++ index.js
@@ -26,12 +26,22 @@ ctx.stroke()
 let canvas2 = document.querySelector('#canvas_2')
 let ctx_2 = canvas2.getContext('2d')
 
-ctx_2.fillStyle = 'red'
-ctx_2.fillRect(10, 10, 90, 80)
+// ctx_2.fillStyle = 'red'
+// ctx_2.fillRect(10, 10, 90, 80)
 
-ctx_2.strokeStyle = 'black'
-ctx_2.rect(100, 10, 90,80)
-ctx_2.lineWidth = '1'
+// ctx_2.strokeStyle = 'black'
+// ctx_2.rect(100, 10, 90,80)
+// ctx_2.lineWidth = '1'
+// ctx_2.fill()
+// ctx_2.stroke()
+
+ctx_2.beginPath()
+ctx_2.fillStyle = 'red';
+ctx_2.strokeStyle = 'blue'
+ctx_2.moveTo(10,10)
+ctx_2.lineTo(150,150)
+ctx_2.lineTo(150,10)
+ctx_2.lineWidth = '5'
 ctx_2.fill()
+ctx_2.closePath()
 ctx_2.stroke()
-
